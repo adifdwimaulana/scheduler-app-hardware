e255ccc46ae4f52635430167286bc447a0a47468
Adif Dwi Maulana <adifdwimaulana@gmail.com>
2020-06-12 15:51:45 +0700

    Add Finger, RTC, LCD

--- Integration/Integration.ino
+++ Integration/Integration.ino
@@ -1,25 +1,48 @@
-// Date and time functions using a DS1307 RTC connected via I2C and Wire lib
+//FirebaseESP8266.h must be included before ESP8266WiFi.h
+#include "FirebaseESP8266.h"
+#include <ESP8266WiFi.h>
 #include "RTClib.h"
 #include <Wire.h> 
 #include <LiquidCrystal_I2C.h>
+#include <Adafruit_Fingerprint.h>
+
+#define WIFI_SSID "KONTRAKAN ANAK2"
+#define WIFI_PASSWORD "bukaopen"
+#define FIREBASE_HOST "finger-scheduler.firebaseio.com" //Without http:// or https:// schemes
+#define FIREBASE_AUTH "Ad5JNpZqepilbQQGgMTE6dzR9DcmiXqDr1cPITjZ"
+
+//Define Firebase Data object
+FirebaseData firebaseData;
+SoftwareSerial mySerial(0, 2); //D3, D4
 
 RTC_DS1307 rtc;
 LiquidCrystal_I2C lcd(0x27, 16, 2);
+Adafruit_Fingerprint finger = Adafruit_Fingerprint(&mySerial);
+
+
+// void printResult(FirebaseData &data);
+String jsonStr;
+//String path = "/1/matkul/selasa";
+String path;
+const int deviceId = 1;
 
 char daysOfTheWeek[7][12] = {"Minggu", "Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu"};
 
-String dayUrl;
-const int deviceId = 1;
-bool isKuliah = false;
+size_t len;
 
-void setup () {
-  Serial.begin(57600);
+// Global vars Data Jadwal dan Dosen
+String aNamaMatkul, aDosen, aStart, aEnd, aRuangan;
+String bNamaMatkul, bDosen, bStart, bEnd, bRuangan;
+String cNamaMatkul, cDosen, cStart, cEnd, cRuangan;
+int matkulCount;
 
-#ifndef ESP8266
-  while (!Serial); // wait for serial port to connect. Needed for native USB
-#endif
+String aDosenId, aDosenNama, aDosenNip;
+String bDosenId, bDosenNama, bDosenNip;
+String cDosenId, cDosenNama, cDosenNip;
 
-  if (! rtc.begin()) {
+void setup()
+{
+    if (! rtc.begin()) {
     Serial.println("Couldn't find RTC");
     Serial.flush();
     abort();
@@ -37,10 +60,10 @@ void setup () {
 
   // When time needs to be re-set on a previously configured device, the
   // following line sets the RTC to the date & time this sketch was compiled
-  // rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+   rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
   // This line sets the RTC with an explicit date & time, for example to set
   // January 21, 2014 at 3am you would call:
-   rtc.adjust(DateTime(2020, 6, 11, 21, 18, 45));
+//   rtc.adjust(DateTime(2020, 6, 11, 21, 18, 45));
 
   lcd.begin();
 
@@ -56,10 +79,226 @@ void setup () {
   lcd.print(deviceId);
   delay(3000);
   lcd.clear();
+
+  Serial.begin(57600);
+  Serial.println();
+  Serial.println();
+
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("Connecting to Wi-Fi");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+      Serial.print(".");
+      delay(300);
+  }
+  Serial.println();
+  Serial.print("Connected with IP: ");
+  Serial.println(WiFi.localIP());
+  Serial.println();
+
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+  Firebase.reconnectWiFi(true);
+
+  //Set the size of WiFi rx/tx buffers in the case where we want to work with large data.
+  firebaseData.setBSSLBufferSize(1024, 1024);
+
+  //Set the size of HTTP response buffers in the case where we want to work with large data.
+  firebaseData.setResponseSize(1024);
+  finger.begin(57600);  
+  if (finger.verifyPassword()) {
+    Serial.println("Found fingerprint sensor!");
+  } else {
+    Serial.println("Did not find fingerprint sensor :(");
+    while (1) { delay(1); }
+  }
+
+  finger.getTemplateCount();
+
+    if (finger.templateCount == 0) {
+    Serial.print("Sensor doesn't contain any fingerprint data. Please run the 'enroll' example.");
+  } 
+  else {
+    Serial.println("Waiting for valid finger...");
+      Serial.print("Sensor contains "); Serial.print(finger.templateCount); Serial.println(" templates");
+  }
+
+
+//    String jsonStr = "";
+//
+//    FirebaseJson json1;
+//
+//    FirebaseJsonData jsonObj;
+//
+//    json1.set("Hi/myInt", 200);
+//    json1.set("Hi/myDouble", 0.0023);
+//    json1.set("Who/are/[0]", "you");
+//    json1.set("Who/are/[1]", "they");
+//    json1.set("Who/is/[0]", "she");
+//    json1.set("Who/is/[1]", "he");
+//    json1.set("This/is/[0]", false);
+//    json1.set("This/is/[1]", true);
+//    json1.set("This/is/[2]", "my house");
+//    json1.set("This/is/[3]/my", "world");
+//
+//    Serial.println("------------------------------------");
+//    Serial.println("JSON Data");
+//    json1.toString(jsonStr, true);
+//    Serial.println(jsonStr);
+//    Serial.println("------------------------------------");
+//
+//    Serial.println("------------------------------------");
+//    Serial.println("Set JSON test...");
+//
+//    if (Firebase.set(firebaseData, path, json1))
+//    {
+//        Serial.println("PASSED");
+//        Serial.println("PATH: " + firebaseData.dataPath());
+//        Serial.println("TYPE: " + firebaseData.dataType());
+//        Serial.print("VALUE: ");
+//        printResult(firebaseData);
+//        Serial.println("------------------------------------");
+//        Serial.println();
+//    }
+//    else
+//    {
+//        Serial.println("FAILED");
+//        Serial.println("REASON: " + firebaseData.errorReason());
+//        Serial.println("------------------------------------");
+//        Serial.println();
+//    }
+
+    Serial.println("------------------------------------");
+    Serial.println("Get JSON test...");
 }
 
-void loop () {
-  // RTC Configuration
+void printResult(FirebaseData &data)
+{
+
+    if (data.dataType() == "int")
+        Serial.println(data.intData());
+    else if (data.dataType() == "float")
+        Serial.println(data.floatData(), 5);
+    else if (data.dataType() == "double")
+        printf("%.9lf\n", data.doubleData());
+    else if (data.dataType() == "boolean")
+        Serial.println(data.boolData() == 1 ? "true" : "false");
+    else if (data.dataType() == "string")
+        Serial.println(data.stringData());
+    else if (data.dataType() == "json")
+    {
+        Serial.println();
+        FirebaseJson &json = data.jsonObject();
+        //Print all object data
+//        Serial.println("Pretty printed JSON data:");
+        json.toString(jsonStr, true);
+//        Serial.println(jsonStr);
+//        Serial.println();
+//        Serial.println("Iterate JSON data:");
+//        Serial.println();
+        len = json.iteratorBegin();
+        String key, value = "";
+        int type = 0;
+//        Serial.print("Size = ");
+//        Serial.println(len);
+        for (size_t i = 0; i < len; i++)
+        {
+            json.iteratorGet(i, type, key, value);
+//            Serial.print(i);
+//            Serial.print(", ");
+//            Serial.print("Type: ");
+//            Serial.print(type == FirebaseJson::JSON_OBJECT ? "object" : "array");
+            if (type == FirebaseJson::JSON_OBJECT)
+            {
+//                Serial.print(", Key: ");
+//                Serial.print(key);
+            }
+//            Serial.print(", Value: ");
+//            Serial.println(value);
+            if(len <= 6){
+              if(i == 0){
+                
+              } else if(i == 1){
+                aDosen = value;
+              } else if(i == 2){
+                aEnd = value;
+              } else if(i == 3){
+                aNamaMatkul = value;
+              } else if(i == 4){
+                aRuangan = value;
+              } else if(i == 5){
+                aStart = value;
+              }  
+            } else if(len > 6 && len <= 12){
+              if(i-6 == 0){
+                
+              } else if(i-6 == 1){
+                bDosen = value;
+              } else if(i-6 == 2){
+                bEnd = value;
+              } else if(i-6 == 3){
+                bNamaMatkul = value;
+              } else if(i-6 == 4){
+                bRuangan = value;
+              } else if(i-6 == 5){
+                bStart = value;
+              }
+            }
+            
+        }
+        json.iteratorEnd();
+    }
+    else if (data.dataType() == "array")
+    {
+        Serial.println();
+        //get array data from FirebaseData using FirebaseJsonArray object
+        FirebaseJsonArray &arr = data.jsonArray();
+        //Print all array values
+        Serial.println("Pretty printed Array:");
+        String arrStr;
+        arr.toString(arrStr, true);
+        Serial.println(arrStr);
+        Serial.println();
+        Serial.println("Iterate array values:");
+        Serial.println();
+        for (size_t i = 0; i < arr.size(); i++)
+        {
+            Serial.print(i);
+            Serial.print(", Value: ");
+
+            FirebaseJsonData &jsonData = data.jsonData();
+            //Get the result data from FirebaseJsonArray object
+            arr.get(jsonData, i);
+            if (jsonData.typeNum == FirebaseJson::JSON_BOOL)
+                Serial.println(jsonData.boolValue ? "true" : "false");
+            else if (jsonData.typeNum == FirebaseJson::JSON_INT)
+                Serial.println(jsonData.intValue);
+            else if (jsonData.typeNum == FirebaseJson::JSON_DOUBLE)
+                printf("%.9lf\n", jsonData.doubleValue);
+            else if (jsonData.typeNum == FirebaseJson::JSON_STRING ||
+                     jsonData.typeNum == FirebaseJson::JSON_NULL ||
+                     jsonData.typeNum == FirebaseJson::JSON_OBJECT ||
+                     jsonData.typeNum == FirebaseJson::JSON_ARRAY)
+                Serial.println(jsonData.stringValue);
+        }
+    }
+}
+
+String getValue(String input, char separator, int index){
+  int found = 0;
+  int strIndex[] = {0, -1};
+  int maxIndex = input.length();
+
+  for(int i=0; i <= maxIndex && found<= index; i++){
+    if(input.charAt(i) == separator || i == maxIndex){
+      found++;
+      strIndex[0] = strIndex[1] + 1;
+      strIndex[1] = (i == maxIndex) ? i+1 : i;
+    }
+  }
+  return found>index ? input.substring(strIndex[0], strIndex[1]) : "";
+}
+
+void loop(){  
   DateTime now = rtc.now();
   int currentYear = now.year();
   int currentMonth = now.month();
@@ -79,30 +318,151 @@ void loop () {
   lcd.print(currentMonth);
   lcd.print("-");
   lcd.print(currentYear);
-
-
   // URL    
   today.toLowerCase();
-
-  String url = 
-
-  if(isKuliah == false) {
-    // Firebase
-    
-    lcd.setCursor(0,1);
-    lcd.print("Menunggu Kuliah");
-  } else {
-    // Firebase
   
-    lcd.setCursor(0,1);
-    lcd.print("Ada Kuliah...");
+  path = "/" + String(deviceId) + "/matkul/" + today;
+//  path = "/" + String(deviceId) + "/matkul/selasa";
+   if (Firebase.get(firebaseData, path)){
+//        Serial.println("PASSED");
+//        Serial.println("PATH: " + firebaseData.dataPath());
+//        Serial.println("TYPE: " + firebaseData.dataType());
+//        Serial.print("VALUE: ");
+        if (firebaseData.dataType() == "json")
+        {
+            jsonStr = firebaseData.jsonString();
+            printResult(firebaseData);
+        }
+//        Serial.println("------------------------------------");
+//        Serial.println();
+    }
+    else
+    {
+        Serial.println("FAILED");
+        Serial.println("REASON: " + firebaseData.errorReason());
+        Serial.println("------------------------------------");
+        Serial.println();
+    }
+    
+    
+    String startHour = getValue(aStart, ':', 0);
+    String startMinute = getValue(aStart, ':', 1);
+    String endHour = getValue(aEnd, ':', 0);
+    String endMinute = getValue(aEnd, ':', 1);
+    bool isAdaDosen = false;
+    if(currentHour >= startHour.toInt() && (currentHour <= endHour.toInt() && currentMinute <= endMinute.toInt())){
+      Serial.print("Mata Kuliah: ");
+      Serial.println(aNamaMatkul);
+      Serial.print("Dosen: ");
+      Serial.println(aDosen);    
+      Serial.print("Start = ");
+      Serial.println(aStart);
+      Serial.print("End Hour = ");
+      Serial.println(aEnd);
+      if(isAdaDosen == false){
+        lcd.setCursor(0,1);
+        lcd.print("Menunggu Dosen...");
+        // Relay Mati
+      } else {
+        // Button Pencet
+        getFingerprintIDez();
+        delay(1000);  
+
+        lcd.setCursor(0,1);
+        lcd.print("Ada Kuliah...");
+
+        // Relay Nyala
+      }
+      
+    } else {
+      Serial.println("TIDAK ADA KULIAH");  
+      lcd.setCursor(0,1);
+      lcd.print("Menunggu Kuliah");
+    }
+    
+    
+  delay(3000);
+}
+
+  uint8_t getFingerprintID() {
+    uint8_t p = finger.getImage();
+    switch (p) {
+      case FINGERPRINT_OK:
+        Serial.println("Image taken");
+        break;
+      case FINGERPRINT_NOFINGER:
+        Serial.println("No finger detected");
+        return p;
+      case FINGERPRINT_PACKETRECIEVEERR:
+        Serial.println("Communication error");
+        return p;
+      case FINGERPRINT_IMAGEFAIL:
+        Serial.println("Imaging error");
+        return p;
+      default:
+        Serial.println("Unknown error");
+        return p;
+    }
+  
+    // OK success!
+  
+    p = finger.image2Tz();
+    switch (p) {
+      case FINGERPRINT_OK:
+        Serial.println("Image converted");
+        break;
+      case FINGERPRINT_IMAGEMESS:
+        Serial.println("Image too messy");
+        return p;
+      case FINGERPRINT_PACKETRECIEVEERR:
+        Serial.println("Communication error");
+        return p;
+      case FINGERPRINT_FEATUREFAIL:
+        Serial.println("Could not find fingerprint features");
+        return p;
+      case FINGERPRINT_INVALIDIMAGE:
+        Serial.println("Could not find fingerprint features");
+        return p;
+      default:
+        Serial.println("Unknown error");
+        return p;
+    }
+    
+    // OK converted!
+    p = finger.fingerFastSearch();
+    if (p == FINGERPRINT_OK) {
+      Serial.println("Found a print match!");
+    } else if (p == FINGERPRINT_PACKETRECIEVEERR) {
+      Serial.println("Communication error");
+      return p;
+    } else if (p == FINGERPRINT_NOTFOUND) {
+      Serial.println("Did not find a match");
+      return p;
+    } else {
+      Serial.println("Unknown error");
+      return p;
+    }   
+    
+    // found a match!
+    Serial.print("Found ID #"); Serial.print(finger.fingerID); 
+    Serial.print(" with confidence of "); Serial.println(finger.confidence); 
+  
+    return finger.fingerID;
   }
   
+  // returns -1 if failed, otherwise returns ID #
+  int getFingerprintIDez() {
+    uint8_t p = finger.getImage();
+    if (p != FINGERPRINT_OK)  return -1;
   
-
-
-
-
-  delay(1000);
-//  lcd.clear();
-}
+    p = finger.image2Tz();
+    if (p != FINGERPRINT_OK)  return -1;
+  
+    p = finger.fingerFastSearch();
+    if (p != FINGERPRINT_OK)  return -1;
+    
+    // found a match!
+    Serial.print("Found ID #"); Serial.print(finger.fingerID); 
+    Serial.print(" with confidence of "); Serial.println(finger.confidence);
+    return finger.fingerID; 
+  }
